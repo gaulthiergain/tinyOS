efe306a15862ae249915333320091fbe3b29daad
Gaulthier Gain <gaulthier.gain@uliege.be>
2020-07-08 20:05:14 +0200

    Add gdt for segmentation
    Signed-off-by: Gaulthier Gain <gaulthier.gain@uliege.be>

---
+++ gdt.h
@@ -0,0 +1,32 @@
+#ifndef _GDT_H
+#define _GDT_H
+
+#define NUM_GDT_ENTRIES 3
+#define SEGMENT_BASE    0
+#define SEGMENT_LIMIT   0xFFFFF
+#define CODE_RX_TYPE    0xA
+#define DATA_RW_TYPE    0x2
+#define PL0             0x0
+#define PL1             0x3
+
+struct gdt{
+    unsigned int	address;
+    unsigned short	size;
+} __attribute__((packed));
+
+struct gdt_entry{
+    unsigned short	limit;
+    unsigned short	base1;
+    unsigned char	base2;
+    unsigned char	dpl;
+    unsigned char	limit2;
+    unsigned char	base3;
+} __attribute__((packed));
+
+typedef unsigned short  gdt_selector;
+
+void gdt_init(void);
+
+extern void load_gdt(struct gdt g);
+
+#endif /* _GDT_H */
--- kmain.c
+++ kmain.c
@@ -1,13 +1,15 @@
 //#include "serial.h"
 #include "fb.h"
+#include "gdt.h"
 
 void kmain(){
     unsigned char s[] = "GauOS";
 
+    gdt_init();
+    
     fb_init();
     fb_clean_screen();
     fb_write(s, sizeof(s));
-    
     //serial_init();                // init serial (with bochs)
     //serial_write(s, sizeof(s));   // write on serial (with bochs)
 
